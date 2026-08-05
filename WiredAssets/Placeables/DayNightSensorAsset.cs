ebd1663c3b1054a696c2ae5bb62cb9084992f197
mewshindo <hellrayz11@gmail.com>
2026-08-05 22:17:11 +0300

    add: day/night sensor logic

---
+++ WiredAssets/Placeables/DayNightSensorAsset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wired.WiredAssets.Placeables
+{
+    public class DayNightSensorAsset : IWiredAsset
+    {
+        public Guid GUID { get; set; }
+        public DayNightSensorMode Mode { get; set; }
+    }
+    public enum DayNightSensorMode
+    {
+        Day,
+        Night
+    }
+}
---
+++ WiredInteractables/DayNightSensor.cs
@@ -0,0 +1,45 @@
+﻿using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Wired.Models;
+using Wired.WiredAssets;
+using Wired.WiredAssets.Placeables;
+
+namespace Wired.WiredInteractables
+{
+    public class DayNightSensor : MonoBehaviour, IWiredInteractable
+    {
+        public Interactable interactable { get; private set; }
+        
+        public bool IsOn { get; private set; }
+
+        private DayNightSensorAsset _asset;
+        private GateNode _gate;
+        public void Start()
+        {
+            _gate = gameObject.GetComponent<GateNode>();
+            _asset = (DayNightSensorAsset)_gate.Asset;
+
+            LightingManager.onDayNightUpdated += onDayNightUpdated;
+        }
+
+        private void onDayNightUpdated(bool isDaytime)
+        {
+            SetPowered(isDaytime == (_asset.Mode == DayNightSensorMode.Day));
+        }
+
+        public void SetPowered(bool state)
+        {
+            
+        }
+
+        public void Uninitialize()
+        {
+            
+        }
+    }
+}
